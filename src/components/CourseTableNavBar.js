3f2c5a0b8bc0396bcbab9bcea9da8cf7e98d9631
rupatel <patel.ru@husky.neu.edu>
2018-09-26 16:03:50 -0400

    Course Table Navbar bug fix

--- src/components/CourseTableNavBar.js
+++ src/components/CourseTableNavBar.js
@@ -8,17 +8,17 @@ const CourseTableNavBar = () => {
     return (
         <nav style={navbarHeight} className="navbar navbar-expand navbar-dark bg-primary fixed-top">
             <i className="fas fa-bars pr-3"></i>
-            <a className="navbar-brand pb-0 pt-0 d-none d-sm-inline" href="#">
-                <p className="navbar-text text-left text-white text-bold m-0">Course Manager</p>
-            </a>
+            <p className="mb-0 navbar-brand d-none d-sm-inline navbar-text text-left text-white text-bold">
+                Course Manager
+            </p>
             <form className="form-inline w-100">
                 <input className="form-control col-10" type="search" placeholder="New Course Title"/>
                 <div className="col-2">
                     <a href="#">
-                            <span className="fa-stack fa-2x">
-                              <i className="fas fa-circle fa-stack-1x circle"></i>
-                              <i className="fas fa-plus fa-stack-1x fa-inverse"></i>
-                            </span>
+                        <span className="fa-stack fa-2x">
+                          <i className="fas fa-circle fa-stack-1x circle"></i>
+                          <i className="fas fa-plus fa-stack-1x fa-inverse"></i>
+                        </span>
                     </a>
                 </div>
             </form>
