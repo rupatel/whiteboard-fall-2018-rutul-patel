bd45b05a56c8e96288e5e4d70ece96c5709e08ae
rupatel <patel.ru@husky.neu.edu>
2018-09-28 20:39:48 -0400

    card grid css fix

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -14,25 +14,23 @@ const CourseCard = () => {
         'of separate logical pages in the application.[2] Interaction with the single page application often involves ' +
         'dynamic communication with the web server behind the scenes.';
     return (
-        <div className="p-0 col-sm-12 col-md-4 col-lg-2" style={{height:'20rem'}}>
-            <div className="col-12 pl-3 pt-5 pb-0 pr-0 h-100">
-                <div className="card-body p-auto h-75" style={{background: "#FFFFFF"}}>
-                    <p className="card-text h-100">{courseDescription}</p>
-                </div>
-                <ul className="list-group list-group-flush h-25">
-                    <li className="list-group-item">
-                        <div className="row">
-                            <h5 className="card-title font-weight-bold text-truncate">{courseTitle}</h5>
-                        </div>
-                        <div className="row">
+        <div className="p-0 col-sm-12 col-md-4 col-lg-2 h-100">
+            <div className="col-12 pl-3 pt-3 pb-0 pr-0 h-100">
+                <div className="h-100" style={{background: "#FFFFFF"}}>
+                    <div className="card-body">
+                        <p className="card-text" style={{maxHeight: "20rem"}}>{courseDescription}</p>
+                    </div>
+                    <div className="h-25 p-2">
+                        <h5 className="card-title font-weight-bold text-truncate">{courseTitle}</h5>
+                        <div className="d-block">
                             <a className="mr-2 no-decorate d-inline" href="/course-editor/course-editor.template.client.html">
                                 <i className="fas fa-file-alt"></i> </a>
                             <p className="mr-2 text-truncate d-inline">Modified 8.09 am</p>
                             <a className="-pull-right no-decorate d-inline" href="/course-editor/course-editor.template.client.html">
                                 <i className="fas fa-ellipsis-v"></i> </a>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
