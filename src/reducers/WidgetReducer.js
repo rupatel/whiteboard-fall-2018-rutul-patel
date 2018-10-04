88e0a181cb6b615590f332fb33f71ebfc04a35cb
rupatel <patel.ru@husky.neu.edu>
2018-10-04 02:42:44 -0400

    reducer cases  implementation

--- src/reducers/WidgetReducer.js
+++ src/reducers/WidgetReducer.js
@@ -4,11 +4,39 @@ const initialState = {
 const WidgetReducer = (state=initialState, action) => {
     switch (action.type) {
         case CREATE_WIDGET:
-        case DELETE_WIDGET:
-        case UPDATE_WIDGET:
-        case FIND_WIDGET:
-        case FIND_ALL_WIDGETS_FOR_TOPIC:
-        case FIND_ALL_WIDGETS:
+        {
+            let newState = {...state}
+            newState.widgets.push(action.widget)
+            return newState;
+        }
+        case DELETE_WIDGET: {
+            let newState = {...state}
+            newState.widgets = newState.widgets.filter(w=> (w.id!=action.widget.id))
+            return newState;
+        }
+        case UPDATE_WIDGET{
+            let newState = {...state}
+            newState.widgets = newState.widgets.map(w=> {
+                if(w.id==action.widget.id) return action.widget
+                else return w;
+            });
+            return newState;
+        }
+        case FIND_WIDGET:{
+            let newState = {...state}
+            newState.widgets = newState.widgets.filter(w=> w.id==action.widget.id);
+            return newState;
+        }
+        case FIND_ALL_WIDGETS_FOR_TOPIC:{
+            let newState = {...state}
+            newState.widgets = action.state.widgets;
+            return newState;
+        }
+        case FIND_ALL_WIDGETS:{
+            let newState = {...state}
+            newState.widgets = action.state.widgets;
+            return newState;
+        }
         default:
             return state
     }
