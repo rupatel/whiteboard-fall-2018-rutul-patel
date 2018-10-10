e3d52e5422af4a46b79b0858302030fa62bc021a
rupatel <patel.ru@husky.neu.edu>
2018-10-09 22:59:49 -0400

    placeholder and onchange for Link Widget

--- src/components/LinkWidgetContent.js
+++ src/components/LinkWidgetContent.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-const LinkWidgetContent = ({widget}) => {
+const LinkWidgetContent = ({widget,updateWidget}) => {
     return (
         <div className="container">
             <input type="text"
                    value={widget.href}
-                   className="form-control mb-2"/>
+                   className="form-control mb-2"
+                   placeholder="Link URL"
+                   onChange={e=> {
+                       let w = {...widget};
+                       w.href = e.currentTarget.value;
+                       updateWidget(w);
+                   }}/>
             <input type="text"
-                   value="linke text"
-                   className="form-control mb-2"/>
-            <input type="text" className="form-control" value={widget.name}/>
+                   value={widget.title}
+                   className="form-control mb-2"
+                   placeholder="Link Text"
+                   onChange={e=> {
+                       let w = {...widget};
+                       w.title = e.currentTarget.value;
+                       updateWidget(w);
+                   }}/>
+            <input type="text" className="form-control" value={widget.name}
+                   placeholder="Widget Name"
+                   onChange={e=> {
+                       let w = {...widget};
+                       w.name = e.currentTarget.value;
+                       updateWidget(w);
+                   }}/>
         </div>
     );
 }
