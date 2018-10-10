ea8c9584c0a34e89fa41ae11f79dcff4090ae81d
rupatel <patel.ru@husky.neu.edu>
2018-10-09 23:02:29 -0400

    placeholder and onchange for Image Widget

--- src/components/ImageWidgetContent.js
+++ src/components/ImageWidgetContent.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const ImageWidgetContent = ({widget}) => {
+const ImageWidgetContent = ({widget, updateWidget}) => {
     return (
         <div className="container">
             <input type="text"
+                   placeholder="Image Source"
                    value={widget.src}
-                   className="form-control mb-2"/>
-            <input type="text" className="form-control" value={widget.name}/>
+                   className="form-control mb-2"
+                   onChange={e=> {
+                       let w = {...widget};
+                       w.src = e.currentTarget.value;
+                       updateWidget(w);
+                   }}/>
+            <input type="text" className="form-control" value={widget.name}
+                   placeholder="Widget Name"
+                   onChange={e=> {
+                       let w = {...widget};
+                       w.name = e.currentTarget.value;
+                       updateWidget(w);
+                   }}/>
         </div>
     );
 }
